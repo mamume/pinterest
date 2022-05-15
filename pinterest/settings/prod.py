3f3099b6f8975514dd26ccbd4c7ea54247733ff9
Mahmoud Metwally <mmustafametwally@gmail.com>
2022-05-15 22:04:32 +0200

    adjust database & email settings

--- pinterest/settings/prod.py
+++ pinterest/settings/prod.py
@@ -1,6 +1,11 @@
+import dj_database_url
 
 from .common import *
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['pinterest-mamume.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
