5f237e813ce341854d0764b27657a6eb1399cac6
Mahmoud Metwally <mmustafametwally@gmail.com>
2022-05-17 00:41:56 +0200

    add build dir to TEMPLATES

--- pinterest/settings/common.py
+++ pinterest/settings/common.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'pinterest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
