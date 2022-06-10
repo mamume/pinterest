0f048bd00687bad56a004129a14e8e8303e88111
Mahmoud Metwally <mmustafametwally@gmail.com>
2022-06-11 01:06:53 +0200

    config static s3

--- pinterest/settings/prod.py
+++ pinterest/settings/prod.py
@@ -10,17 +10,25 @@ ALLOWED_HOSTS = ['pinterest-mamume.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500, ssl_require=True)
 }
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_URL = os.environ.get('AWS_URL')
-AWS_DEFAULT_ACL = None
+# AWS_URL = os.environ.get('AWS_URL')
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+AWS_LOCATION = 'static'
+
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_ADDRESSING_STYLE = "virtual"
 
-STATIC_URL = AWS_URL + '/static/'
+# STATIC_URL = AWS_URL + '/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = AWS_URL + '/media/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-django_on_heroku.settings(locals(), staticfiles=False)
+
+# MEDIA_URL = AWS_URL + '/media/'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# django_on_heroku.settings(locals(), staticfiles=False)
