466377a58ba14c2841578219c431fff201faa66b
Mahmoud Metwally <mmustafametwally@gmail.com>
2022-06-11 02:19:31 +0200

    fix static assets relative url

--- pinterest/settings/common.py
+++ pinterest/settings/common.py
@@ -111,7 +111,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
-    os.path.join(BASE_DIR, 'build'),
+    # os.path.join(BASE_DIR, 'build'),
     # os.path.join(BASE_DIR, 'static'),
 ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
